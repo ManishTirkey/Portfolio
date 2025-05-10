f3a25f9886a671d1c135f3f0b6d0090372b99368
asdmamanish <asdmamanish@gmail.com>
2025-05-10 16:04:01 +0000

    added configuration to deploy in pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: '/glass-folio-itune-vibes/',
-  // base: '/new/',
+  base: '/Portfolio/',
   server: {
     host: "::",
     port: 8080,
